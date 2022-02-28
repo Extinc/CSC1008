76ff02601a14fb7935924ae0b5741bfc062b49f7
VoidBlanc <20370099+voidblanc@users.noreply.github.com>
2022-02-28 10:27:31 +0800

    testing onemap api & Split login / register template to a separate file for ease modification
    Signed-off-by: = <=>

--- Lift/views.py
+++ Lift/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
-
+onemapdev_url = "https://developers.onemap.sg"
 # Create your views here.
 def index(request):
-    response = requests.get(
-        "https://developers.onemap.sg/commonapi/search?searchVal=revenue&returnGeom=Y&getAddrDetails=Y&pageNum=1")
-    response1 = requests.get(
-        "https://developers.onemap.sg/privateapi/popapi/getPlanningarea?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjgsInVzZXJfaWQiOjgsImVtYWlsIjoiY2xvc2Vkc3VwZXJhcGl1c2VyQHNsYS5nb3Yuc2ciLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cDpcL1wvMTAuMC4zLjExOjgwODBcL2FwaVwvdjJcL3VzZXJcL3Nlc3Npb24iLCJpYXQiOjE0NjczNjA0NjEsImV4cCI6MTQ2Nzc5MjQ2MSwibmJmIjoxNDY3MzYwNDYxLCJqdGkiOiJjNzkwZjBhYjQwOTcwNzFhMWE4MDQ1YTFjNjRlM2M5MSJ9.Pp6zos_p_jjiChWsi2F7O-k-yxgr173QlXqeGc-rpWw&lat=1.3&lng=103.8")
+    urls = onemapdev_url+ "/privateapi/popapi/getAllPlanningarea"
+    params= {}
+    params['token'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjg0NjUsInVzZXJfaWQiOjg0NjUsImVtYWlsIjoiYWxwaGF0b29uQGhvdG1haWwuY29tIiwiZm9yZXZlciI6ZmFsc2UsImlzcyI6Imh0dHA6XC9cL29tMi5kZmUub25lbWFwLnNnXC9hcGlcL3YyXC91c2VyXC9zZXNzaW9uIiwiaWF0IjoxNjQ1OTQ1NjI5LCJleHAiOjE2NDYzNzc2MjksIm5iZiI6MTY0NTk0NTYyOSwianRpIjoiMmMyZjA1MjU0MTkwZDVkMTYyYTZkZDI1OGY1OGJiODAifQ._fQ4y6Od1uFnftfElyemT1yEiraOX-LX-5Ojy6NHJaY'
+    params['lat'] = '1.3776586388017433'
+    params['lng'] = '103.84874408446957'
+    response = requests.get(urls, params=params)
     data = response.json();
     # data1 = response1.json();
     args = {}
     args['title'] = "Home"
+    args['testdata'] = data
     if request.user.is_authenticated:
         fname = request.user.first_name
         args['fname'] = fname
         return render(request, 'index.html', args)
     else:
-        return render(request, 'index.html', {"title": "Home"})
+        return render(request, 'index.html', args)
     # return HttpResponse(data['results'][0]['LONGITUDE'])
