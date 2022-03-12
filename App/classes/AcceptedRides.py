856434e09be40bdabc3c3e52eaf9ddac94bfa88e
bolo56moto <bolo56moto1@gmail.com>
2022-03-13 00:05:16 +0800

    Created new classes

---
+++ App/classes/AcceptedRides.py
@@ -0,0 +1,11 @@
+class AcceptedRides:
+  def __init__(self, rideId,passengerId,pickUpLocation,pickUpTime,destination,distance,price,typeOfCar,driverId):
+    self.rideId = rideId
+    self.passengerId= passengerId
+    self.pickUpLocation = pickUpLocation
+    self.pickUpTime =pickUpTime
+    self.destination = destination
+    self.distance = distance
+    self.price = price
+    self.typeOfCar = typeOfCar
+    self.driverId = driverId
---
+++ App/classes/Driver.py
@@ -0,0 +1,6 @@
+class Driver:
+  def __init__(self, userId, driverLocation,typeOfCar):
+    self.name = userId
+    self.driverLocation = driverLocation
+    self.typeOfCar = typeOfCar
+  
