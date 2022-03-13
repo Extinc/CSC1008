a79902a7979147cf6cac3a713e048d39c68edc4d
bolo56moto <bolo56moto1@gmail.com>
2022-03-13 14:54:13 +0800

    Update 2 sorry :(

--- App/classes/AcceptedRides.py
+++ App/classes/AcceptedRides.py
@@ -1,12 +1,15 @@
 class AcceptedRides:
-  def __init__(self, rideId,passengerId,pickUpLocation,pickUpTime,destination,driverDistance,rideDistance,price,typeOfCar,driverId):
+  def __init__(self, rideId,passengerId,pickUpLocation,driverLocation,pickUpTime,destination,rideDistance,price,typeOfCar,driverId):
     self.rideId = rideId
     self.passengerId= passengerId
     self.pickUpLocation = pickUpLocation
     self.pickUpTime =pickUpTime
     self.destination = destination
-    self.driverDistance = driverDistance
+    self.driverDistance = getDriverDistance(driverLocation,pickUpLocation)
     self.rideDistance = rideDistance
     self.price = price
     self.typeOfCar = typeOfCar
     self.driverId = driverId
+
+    def getDriverDistance(driverLocation,pickUpLocation):
+      return driverLocation-pickUpLocation
