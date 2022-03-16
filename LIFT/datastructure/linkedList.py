97993da8b23fc8406860eb674db4a03f46362e80
bolo56moto <bolo56moto1@gmail.com>
2022-03-16 22:43:05 +0800

    NEW data structure

---
+++ LIFT/datastructure/linkedList.py
@@ -0,0 +1,92 @@
+class SinglyListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+    #return the value of the node at index
+
+    def search(self, index):
+        temp = self.head
+        prev = None
+        counter = 0
+        while temp is not None and counter < index:
+            prev = temp
+            temp = temp.next
+            counter += 1
+
+        if temp is None:
+            print('search error: invalid index')
+        else:
+            return temp
+
+    def insertAtHead(self, node):
+        if self.head is None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+
+    def delete(self, value):
+        prev = None
+        temp = self.head
+
+        while temp != None and temp.data != value:
+            prev = temp
+            temp = temp.next
+
+        #node to be deleted is head
+        if temp == self.head:
+            self.deleteAtHead()
+
+        #Value found
+        elif temp != None:
+            prev.next = temp.next
+            del temp
+        #Value not found
+        else:
+            print('Value ', value, ' cannot be found')
+
+    #delete the node at index
+    def deleteAt(self,index):
+        temp = self.head
+        prev = None
+        counter = 0
+        while temp is not None and counter < index:
+            prev = temp
+            temp = temp.next
+            counter += 1
+
+        if temp is None:
+            print('search error: invalid index')
+        else:
+            if prev is None:
+                self.head = temp.next
+            else:
+                prev.next = temp.next
+            del temp
+
+    def deleteAtHead(self):
+        temp = self.head
+        self.head = self.head.next
+        del temp
+
+    def printList(self):
+        output = "Current list content: [ "
+        temp = self.head
+        while temp is not None:
+            output += str(temp.data) + " "
+            temp = temp.next
+        output += "]"
+        print(output)
+
+    #return the number of elements in the queue
+    def size(self):
+        temp = self.head
+        size = 0
+        while temp is not None:
+            size += 1
+            temp = temp.next
+        return size
