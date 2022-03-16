3efdbcb4575fe7c49d0cb1c43de83df8f60d2e0f
VoidBlanc <20370099+voidblanc@users.noreply.github.com>
2022-03-16 21:35:31 +0800

    Updated some settings for my route
    Signed-off-by: VoidBlanc <angelic.noted_0g@icloud.com>

--- LIFT/codes/Routes.py
+++ LIFT/codes/Routes.py
@@ -1,14 +1,17 @@
 import json
 import pandas as pd
+
+from LIFTMAIN.settings import DRIVE_EDGE_DATA, DRIVE_NODE_DATA
+
 edge_data = []
 node_data = []
-f = open('LIFTMAIN/Database/drive_edge.geojson')
+f = open(DRIVE_EDGE_DATA)
 data = json.load(f)
 for feature in data["features"]:
     edge_data.append({"source": feature["properties"]["source"], "dest": feature["properties"]["dest"], "length": feature["properties"]["length"], "highway": feature["properties"]["highway"], "geometry": feature["geometry"]["coordinates"]})
 f.close()
 
-f = open('LIFTMAIN/Database/drive_node.geojson')
+f = open(DRIVE_NODE_DATA)
 data = json.load(f)
 for feature in data["features"]:
     node_data.append({"id": feature["properties"]["id"], "highway": feature["properties"]["highway"], "x": feature["properties"]["x"], "y": feature["properties"]["y"], "geometry_type": feature["geometry"]["type"], "geometry": feature["geometry"]["coordinates"]})
