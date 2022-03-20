3c57c898ccd548cd4dca6dda3e2e9fea935d4294
izzdanial <41255403+izzdanial99@users.noreply.github.com>
2022-03-21 02:12:00 +0800

    fixed indentation issue

--- LIFT/views/homeview.py
+++ LIFT/views/homeview.py
@@ -65,13 +65,13 @@ def plot_route(request):
             # print(geom['coordinates'])
         return JsonResponse(geom, safe=False)
     
-    #get lon n lat of user using ip addr
-    def select_pickup(request):
-        ip = requests.get('https://api.ipify.org?format=json')
-        ip_data = json.loads(ip.text)
-        res = requests.get('http://ip-api.com/json/'+ip_data['ip'])
-        location_data_one = res.text
-        location_data = json.loads(location_data_one)
-        print("lat: " + str(location_data["lat"]))
-        print("lon: " + str(location_data["lon"]))
-        return render(request,'index.html',{'location_data':location_data})
+#get lon n lat of user using ip addr
+def select_pickup(request):
+    ip = requests.get('https://api.ipify.org?format=json')
+    ip_data = json.loads(ip.text)
+    res = requests.get('http://ip-api.com/json/'+ip_data['ip'])
+    location_data_one = res.text
+    location_data = json.loads(location_data_one)
+    print("lat: " + str(location_data["lat"]))
+    print("lon: " + str(location_data["lon"]))
+    return render(request,'index.html',{'location_data':location_data})
