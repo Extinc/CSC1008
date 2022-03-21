74c75897f0898f0229f98a4020b253f6b83acd0c
VoidBlanc <angelic.noted_0g@icloud.com>
2022-03-21 15:57:57 +0800

    Updated some models

--- LIFT/models/models.py
+++ LIFT/models/models.py
@@ -8,5 +8,6 @@ class PointInfo(models.Model):
     ROAD = models.CharField(max_length=250, null=True)
     FEATURENAME = models.CharField(max_length=250, null=True)
     POSTALCODE = models.CharField(max_length=250, null=True)
+    HIGHWAY = models.TextField(null=True)
     def __str__(self):
         return '{} by {}'.format(self.BUILDINGNAME, self.id)
