6c296f0f98bfc055669986849139afd1f0b38bd3
VoidBlanc <angelic.noted_0g@icloud.com>
2022-03-25 09:17:01 +0800

    Added some animation and updated database

--- LIFT/datastructure/Trie.py
+++ LIFT/datastructure/Trie.py
@@ -1,7 +1,38 @@
+from collections import defaultdict
+
+
 class TrieNode:
     def __init__(self):
-        self.next = {}
-        self.leaf = False  # Indicates no other Leaf Node
+        self.isEnd = False
+        self.child = {}
+
 
 class Trie:
-    pass
+    __slots__ = ('children', 'freq', 'name', 'top5')
+
+    def __init__(self):
+        self.children = defaultdict(Trie)
+        self.freq = 0
+        self.name = None
+        self.top5 = []
+
+    def __getitem__(self, suffix):
+        node = self
+        for letter in suffix:
+            node = node.children[letter]
+        return node
+
+    def computetop5(self):
+        candidates = []
+        for letter, child in self.children.items():
+            child.computetop5()
+            candidates.extend(child.top5)
+        if self.name is not None:
+            candidates.append((self.freq, self.name))
+        candidates.sort(reverse=True)
+        self.top5 = candidates[:5]
+
+    def insert(self, freq, name):
+        node = self[name]
+        node.freq += freq
+        node.name = name
