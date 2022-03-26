6a3bff65bdcaa08deb9676e74cd6f951675f5d40
bolo56moto <bolo56moto1@gmail.com>
2022-03-26 23:15:13 +0800

    Test

--- LIFT/datastructure/linkedList.py
+++ LIFT/datastructure/linkedList.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self,driver):
-        details = driver.__dict__
+    def __init__(self,object):
+        details = object.__dict__
         d = details.values()
         list = ' '.join(str(val) for val in d)
         
@@ -49,6 +49,41 @@ class SinglyLinkedList:
         else:
             NewNode.next = self.head
             self.head = NewNode
+            
+    def insertAt(self, object, index):     
+
+        #1. allocate node to new element
+        newNode = Node(object)
+
+        #2. check if the index is > 0 
+        if(index < 1):
+            print("\nindex should be >= 1.")
+        elif (index == 1):
+
+        #3. if the index is 1, make next of the
+        #   new node as head and new node as head
+            newNode.next = self.head
+            self.head = newNode
+        else:    
+
+            #4. Else, make a temp node and traverse to the 
+            #   node previous to the index
+            temp = self.head
+            for i in range(1, index-1):
+                if(temp != None):
+                    temp = temp.next   
+
+            #5. If the previous node is not null, make 
+            #   newNode next as temp next and temp next 
+            #   as newNode.
+            if(temp != None):
+                newNode.next = temp.next
+                temp.next = newNode  
+            else:
+
+                #6. When the previous node is null
+                print("\nThe previous node is null.")  
+
 
     def delete(self, value):
         prev = None
