8fecdcfea04eb904bd4c59534e592828a58ab0e2
VoidBlanc <angelic.noted_0g@icloud.com>
2022-04-06 20:32:18 +0800

    added some validation
    Signed-off-by: VoidBlanc <angelic.noted_0g@icloud.com>

--- LIFT/urls.py
+++ LIFT/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
 from .codes import BookingFunctions
+from .views import api
 from .views import homeview, loginview
-from .views import testview, api
 
 urlpatterns = [
     path('', homeview.landing_page, name="landing"),
     path('index', homeview.index, name="HomePage"),
-    path('test', testview.testpage, name="TestPage"),
     path('getInfo/', api.getInfo),
     path('getPrice/', api.getPrice),
     path('findnearest/', api.getNearest),  # API CALL FOR FINDING NEAREST POINT TO USER
@@ -15,7 +14,6 @@ urlpatterns = [
     path('findDriver/', api.findDriver),
     path('get_addr/', api.get_address),
     path('endRide/', BookingFunctions.endRide),
-    path('select_pickup/', testview.select_pickup),
     path('login', loginview.signin, name="Login"),
     path('register', loginview.register, name="Register"),
     path('signout', loginview.signout, name="Logout"),
